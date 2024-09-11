7629aa63bb322054c819da9ec7320ae93eb28212
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-09-11 17:03:38 -0300

    Quarta aula

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,86 @@
+/*AULA 4*/
+
+//PROCESSAMENTO ASSÍNCRONA
+//Promises
+//async/await
+
+//Inferno de callbacks/código hadouken
+// const fs = require('fs')
+// const abrirArquivo = function(nomeArquivo) {
+//     const exibirConteudo = function(erro, conteudo){
+//         if(erro)
+//             console.log(`Erro: ${erro}`)
+//         else {
+//             console.log(`Conteúdo: ${conteudo.toString()}`)
+//             const dobro = Number(conteudo.toString()) * 2
+//             const finalizar = (erro) => {
+//                 if(erro)
+//                     console.log(`Erro na escrita: ${erro}`)
+//                 else
+//                     console.log(`Escreveu com sucesso`)
+//             }
+//             fs.writeFile('dobro.txt', dobro.toString(), finalizar)
+//         }
+//     }
+//     fs.readFile(nomeArquivo, exibirConteudo)
+//     console.log('Fazendo outras coisas...')
+// }
+// abrirArquivo('Arquivo.txt')
+
+/*------------------------------------------ */
+
+//PROCESSAMENTO SINCRONA E ASSINCRONA
+// function demorada(){
+//     const dataAtualMais2Segundos = new Date().getTime() + 2000
+//     while(new Date().getTime() <= dataAtualMais2Segundos);
+//     const d = 2 + 5
+//     return d
+// }
+// const a = 2 + 3
+// const b = 8 + 9
+// setTimeout(() => {
+//     const d = demorada()
+//     console.log(`Demorada: ${d}`)
+// }, 0)
+// const e = a + b + 5
+// const atualMais10 = new Date().getTime() + 10000
+// while (new Date().getTime() <= atualMais10);
+// console.log(e)
+
+/*------------------------------------------ */
+
+//PROCESSAMENTO SINCRONA/BLOQUEANTE
+//EXEMPLO 2
+// const a = 2 + 7
+// const b = 6 + 7
+// cosole.log(a+b)
+
+//EXEMPLO 1
+// console.log('Eu primeiro')
+// console.log('Depois eu')
+// console.log('Serei sempre a última...:(')
+
+/*------------------------------------------ */
+
+
+
 /*AULA 3*/
 //OBJETOS JavaScript LITERAIS (JSON: Javascript Object Notation)
 //EXEMPLO 5. 
 //Ter a operação somar, que soma dois válores, implementada com uma arrow function que não usa return.
 //Ter a operação subtrair, que subtrai dois válores, implementada com uma function regular
-let calculadora = {
+// let calculadora = {
     //Operação de somar usando uma arrow function
-    somar: (a, b) => a + b,
+    // somar: (a, b) => a + b,
     //Operação de subtrair usando uma function regular
-    subtrair: function(a, b) {
-        return a - b
+    // subtrair: function(a, b) {
+    //     return a - b
         //pode ser usado return this.somar(a, -b)
-    }
-}
+//     }
+// }
 
-console.log(calculadora.somar(2, 2))
-console.log(calculadora.subtrair(5, 5))
+// console.log(calculadora.somar(2, 2))
+// console.log(calculadora.subtrair(5, 5))
 
 
 /*EXEMPLO 4. Representar: Uma concessionária tem CNPJ e endereço, com rua, numero e bairro.
